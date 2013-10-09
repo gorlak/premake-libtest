026161dd0573a123c8cbb90071ad7f8684eedeff
Geoff Evans <geoff@flummoxed.org>
2013-10-08 20:19:19 -0700

    working  linker settings on the executable in linux

--- premake5.lua
+++ premake5.lua
@@ -27,12 +27,20 @@ project "bar"
 	files { "bar/*.cpp" }
 	links { "foo" }
 
-if _OPTIONS[ "fix"] then
-	linkoptions { "-Wl,-install_name,@executable_path/libbar.dylib" }
+if _OPTIONS[ "fix" ] then
+	configuration "macosx" linkoptions { "-Wl,-install_name,@executable_path/libbar.dylib" }
 end
 
 project "baz"
 	language "C++"
 	kind "ConsoleApp"
 	files { "baz/*.cpp" }
+
+if _OPTIONS[ "fix" ] then
+	configuration "macosx" links { "foo", "bar" }
+	configuration "linux" links { "foo" }
+	configuration { "linux", "debug" } linkoptions { "-Wl,-rpath=\\$$ORIGIN", "-Lbin/debug", "-lbar" }
+	configuration { "linux", "release" } linkoptions { "-Wl,-rpath=\\$$ORIGIN", "-Lbin/release", "-lbar" }
+else
 	links { "foo", "bar" }
+end
